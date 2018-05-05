34a1b36719dab7333ee7920d1f85f7c8b2249e35
ongaeshi <ongaeshi0621@gmail.com>
2018-05-06 00:35:49 +0900

    Revert

--- main.rb
+++ main.rb
@@ -1,14 +1,16 @@
 class Panel
   OFFSET = 2
 
-  def initialize
+  def initialize(x, y)
+    @x = x
+    @y = y
   end
 
   def update
   end
 
-  def draw(x, y)
-    RoundRect.new(x+OFFSET, y+OFFSET, 30-OFFSET*2, 30-OFFSET*2, 4).draw([240, 240, 240])
+  def draw
+    RoundRect.new(@x+OFFSET, @y+OFFSET, 30-OFFSET*2, 30-OFFSET*2, 4).draw([240, 240, 240])
   end
 
   def open
@@ -23,15 +25,20 @@ end
 
 class Ground
   def initialize(width, height, x_offset = 0, y_offset = 0)
-    @width = width
-    @height = height
-    @x_offset = x_offset
-    @y_offset = y_offset
+    @panels = []
     @table = Array.new(width) { Array.new(height) }
 
     # init
-    each do |x, y|
-      @table[x][y] = Panel.new()
+    0.upto(height-1) do |y|
+      0.upto(width-1) do |x|
+        panel = Panel.new(
+          x_offset + x * 30,
+          y_offset + y * 30
+        )
+
+        @panels << panel
+        @table[x][y] = panel
+      end
     end
 
     # set bomb
@@ -39,28 +46,16 @@ class Ground
     # calc number
   end
 
-  def each
-    0.upto(@height-1) do |y|
-      0.upto(@width-1) do |x|
-        yield x, y
-      end
-    end
-  end
-
   def panel(x, y)
     @table[x][y]
   end
 
   def update
-    each do |x, y|
-      panel(x, y).update
-    end
+    @panels.each { |e| e.update }
   end
 
   def draw
-    each do |x, y|
-      panel(x, y).draw(@x_offset + x*30, @y_offset + y*30)
-    end
+    @panels.each { |e| e.draw }
   end
 end
 
